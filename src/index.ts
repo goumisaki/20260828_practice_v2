2876998bfd99af09ece7c3c28408dcb3d35aa1bf
goumisaki <TTC2450ST0007@edu.tech.ac.jp>
2026-08-28 15:23:07 +0900

    fix:サーバーの初期設定

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,24 @@
-const express = require('express')
+import express, { Request, Response } from 'express';
+import fs from 'fs/promises';
+import path from 'path';
+
+
 const app = express();
-const port = 3000
+const port: number = 3000;
 
 
-//標準モジュールの読み込み
-const fs = require('fs').promises; // ファイルシステムモジュールの読み込み
-const path = require('path');     // パス操作モジュールの読み込み
+// 設定
+app.use(express.urlencoded({ extended: true })); // POST通信設定
+app.use(express.static(path.join(__dirname, 'public'))); // 静的ファイルの指定
+app.set('view engine', 'ejs'); // テンプレートエンジンにEJSを設定
+app.set('views', path.join(__dirname, 'views')); // ビューのディレクトリを設定
 
 
-//設定
-app.use(express.urlencoded({ extended: true }));//POST通信設定
-app.use(express.static( path.join( __dirname, 'public') )); //静的ファイルの指定
-app.set('view engine', 'ejs'); //テンプレートエンジンにEJSを設定
-app.set('views', path.join( __dirname, 'views') ); //ビューのディレクトリを設定
+app.get('/', (req: Request, res: Response): void => {
+  res.send('Hello World!');
+});
 
 
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.listen(port, (): void => {
+  console.log(`Example app listening on port ${port}`);
+});
